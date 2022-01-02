242ee4b9df6555c374478588381dd46c2e528c83
Hector Solis <hsolis11@outlook.com>
2022-01-02 04:28:58 -0600

    adding temp data for testing

--- pokedex/routes.py
+++ pokedex/routes.py
@@ -3,4 +3,6 @@ from pokedex import app
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    repo = database.Pokemons()
+    pokemon = repo.get(id=4)
+    return render_template('stats.html', pokemon=pokemon, image='charmander.png')
