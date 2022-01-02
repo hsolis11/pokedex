eced031df310b05674f8239bd784ada3de6f2a99
Hector Solis <hsolis11@outlook.com>
2022-01-02 04:55:42 -0600

    adding POST method to index

--- pokedex/routes.py
+++ pokedex/routes.py
@@ -3,7 +3,7 @@ import database
 from flask import render_template, request
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
     repo = database.Pokemons()
     pokemon = repo.get(id=4)
